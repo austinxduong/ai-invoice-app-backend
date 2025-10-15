3d860631432fc686b42d8013c4c912d0527b32cb
austinxduong <austinxduong@gmail.com>
2025-10-14 17:15:58 -0700

    create post request to authenticate/register user and its unique _id

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,29 @@ exports.registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "please fill out all fields" });
+        }
+
+        //checks if user already exists
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists "});
+        }
+
+        // create user
+        const user = await User.create({ name, email, password });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(400).json({ message: "invalid user data" });
+        }
     } catch (error) {
         res.status(500).json({ message: "Server error" });
     }
@@ -38,7 +61,7 @@ exports.getMe = async (req, res) => {
 
 // update user profile
 exports.updateUserProfile = async (req, res) => {
-    
+
     try {
     } catch (error) {
         res.status(500).json({ message: "Server error" });
