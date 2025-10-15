0ddfbb14e76f39087eb7b435dafd3475bbe622fb
austinxduong <austinxduong@gmail.com>
2025-10-14 17:51:37 -0700

    seeded database with authenticated user

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,6 +45,22 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
+        const user = await User.findOne({ email }).select("+password");
+
+        if (user && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id),
+
+                businessName: user.businessName || "",
+                address: user.address || "",
+                phone: user.phone || "",
+            });
+        } else {
+            res.status(401).json({ message: "invalid credentials"});
+        }
     } catch (error) {
         res.status(500).json({ message: "Server error" });
     }
