6ae0220eb4246015fff6cb979b81597d9ab5c129
austinxduong <austinxduong@gmail.com>
2025-11-07 16:41:44 -0800

    enable cors for all requests

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const app = express();
 
 
 app.use(
-    cors({
-        origin: ["*"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        
-    })
+    cors()
 );
 
 //connect database
