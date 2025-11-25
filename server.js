b0d03b9bcc26f9faa21d8ac6dc689c1cc05e099e
austinxduong <austinxduong@gmail.com>
2025-11-24 17:18:11 -0800

    reverse cors addition

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const aiRoutes = require('./routes/aiRoutes')
 const app = express();
 
 app.use(
-    cors({
-        origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"],
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true
-    })
+    cors()
 );
 
 //connect database
