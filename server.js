1a88b91983e2ce8470862c94f75f7ef22885b6df
austinxduong <austinxduong@gmail.com>
2025-11-24 17:14:13 -0800

    cors

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const app = express();
 
 app.use(
     cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
     })
 );
 
