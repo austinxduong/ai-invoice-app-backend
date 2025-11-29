7dcf7f3dfedcd8366dca69b9ae2374075abcb339
austinxduong <austinxduong@gmail.com>
2025-11-28 17:52:32 -0800

    testing1

--- config/db.js
+++ config/db.js
@@ -1,13 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.error("Error connecting to MongoDB", err);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 60000,
+            connectTimeoutMS: 60000,
+            socketTimeoutMS: 60000,
+            maxPoolSize: 1,
+            family: 4,                    // ← Force IPv4 only
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error);
         process.exit(1);
     }
 };
 
 module.exports = connectDB;
+
